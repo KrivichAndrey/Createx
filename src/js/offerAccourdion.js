f8803aeea6364e635ae15501387f94c2c6551ab3
Andrey_Krivich <109085432+KrivichAndrey@users.noreply.github.com>
2023-03-31 10:12:18 +0300

    feat: add page "about"

--- src/js/offerAccourdion.js
+++ src/js/offerAccourdion.js
@@ -12,7 +12,9 @@ export const offerAccourdions = () => {
 
         if (e.currentTarget.classList.contains('offerAccourdion_open')) {
           control.setAttribute('aria-expanded', true);
+
           content.setAttribute('aria-expanded', false);
+
           content.style.maxHeight = `${content.scrollHeight}px`;
         } else {
           control.setAttribute('aria-expanded', false);
